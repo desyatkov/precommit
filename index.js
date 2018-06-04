51c87214da7804b9745279fdca5b630b524ee42c
Sergey Desyatkov <Desyatkov@gmail.com>
2018-06-04 16:56:58 +0300

    return 0

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const log = console.log;
 
 const checkStatus =  status => {
     if(status == true) {
-        log( chalk.bgHex('#128731').keyword('black').bold(' Success! ') );
         process.exitCode = 0;
     } else {
-        log( chalk.bgHex('#ffa500').keyword('black').bold(' Error! ') );
+        log( `🚑  ${chalk.bgHex('#ffa500').keyword('black').bold(' GIT COMMIT ERROR! ')}` );
         process.exitCode = 2; 
     }
 }
 
-checkStatus(true)
+checkStatus(true);
